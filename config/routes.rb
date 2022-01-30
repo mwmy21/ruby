ac6fd6b35eb0fd3239a32659725a8763bd2eb1e6
mwmy21 <watam0913@gmail.com>
2022-01-30 23:56:42 +0900

    StaticPage Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :microposts
   resources :users
-  root 'users#index'
+  root 'application#hello'
 end
