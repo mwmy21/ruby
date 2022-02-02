178e0a6f81c5e1ded83fe37c6aae4828792ca67f
mwmy21 <watam0913@gmail.com>
2022-02-02 23:59:43 +0900

    静的ページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   resources :microposts
   resources :users
-  root 'application#hello'
+  root 'static_pages#home'
 end
